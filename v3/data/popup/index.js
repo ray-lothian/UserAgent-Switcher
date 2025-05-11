f608eb9190f3ea23b5bfea9605e3a7136050a1e3
Ray Lothian <ray.lothian@yahoo.com>
2025-05-10 22:47:49 -0700

    Removes container support for now. Preparing FireFox release; based on #222

--- v3/data/popup/index.js
+++ v3/data/popup/index.js
@@ -38,33 +38,7 @@ document.querySelectorAll('[data-localize]').forEach(e => {
   }
 });
 
-const DCSI = 'firefox-default';
-
 document.body.dataset.android = navigator.userAgent.indexOf('Android') !== -1;
-
-let tab = {};
-
-chrome.tabs.query({
-  active: true,
-  currentWindow: true
-}, tbs => {
-  if (tbs.length) {
-    tab = tbs[0];
-    if ('cookieStoreId' in tab) {
-      const apply = document.querySelector('[data-cmd="apply"]');
-      apply.value = chrome.i18n.getMessage('applyContainer');
-      apply.title = chrome.i18n.getMessage('applyContainerTitle');
-
-      const w = document.querySelector('[data-cmd="tab"]');
-      w.value = chrome.i18n.getMessage('applyContainerTab');
-      w.title = chrome.i18n.getMessage('applyContainerTabTitle');
-
-      const reset = document.querySelector('[data-cmd="reset"]');
-      reset.value = chrome.i18n.getMessage('resetContainer');
-      reset.title = chrome.i18n.getMessage('resetContainerTitle');
-    }
-  }
-});
 
 const map = {};
 
@@ -295,64 +269,20 @@ document.addEventListener('click', ({target}) => {
         msg(chrome.i18n.getMessage('msgUASet'));
       }
       if (value !== navigator.userAgent) {
-        // prevent a container ua string from overwriting the default one
-        if ('cookieStoreId' in tab && tab.cookieStoreId !== DCSI) {
-          chrome.runtime.sendMessage({
-            method: 'request-update',
-            value,
-            cookieStoreId: tab.cookieStoreId
-          });
-          chrome.storage.local.get({
-            'container-uas': {}
-          }, prefs => {
-            prefs['container-uas'][tab.cookieStoreId] = value;
-            chrome.storage.local.set(prefs);
-          });
-        }
-        else {
-          chrome.storage.local.set({
-            ua: value
-          });
-        }
+        chrome.storage.local.set({
+          ua: value
+        });
       }
-    }
-    else if (cmd === 'tab') {
-      const value = document.getElementById('ua').value;
-      chrome.storage.session.set({
-        [tab.id]: {
-          ua: value,
-          cookieStoreId: tab.cookieStoreId
-        }
-      });
     }
     else if (cmd === 'reset') {
       const input = document.querySelector('#list :checked');
       if (input) {
         input.checked = false;
       }
-      // prevent a container ua string from overwriting the default one
-      if ('cookieStoreId' in tab && tab.cookieStoreId !== DCSI) {
-        chrome.runtime.sendMessage({
-          method: 'request-update',
-          value: '',
-          cookieStoreId: tab.cookieStoreId,
-          delete: true
-        });
-        chrome.storage.local.get({
-          'container-uas': {}
-        }, prefs => {
-          delete prefs['container-uas'][tab.cookieStoreId];
-          chrome.storage.local.set(prefs);
-        });
-
-        msg(chrome.i18n.getMessage('msgDisabledOnContainer'));
-      }
-      else {
-        chrome.storage.local.set({
-          ua: ''
-        });
-        msg(chrome.i18n.getMessage('msgDisabled'));
-      }
+      chrome.storage.local.set({
+        ua: ''
+      });
+      msg(chrome.i18n.getMessage('msgDisabled'));
     }
     else if (cmd === 'refresh') {
       chrome.tabs.query({
@@ -410,23 +340,3 @@ document.getElementById('ua').addEventListener('keyup', e => {
     document.querySelector('[data-cmd="apply"]').click();
   }
 });
-
-/* container support */
-document.querySelector('[data-cmd="container"]').addEventListener('click', () => {
-  chrome.permissions.request({
-    permissions: ['cookies']
-  }, granted => {
-    if (granted) {
-      window.close();
-    }
-  });
-});
-if (/Firefox/.test(navigator.userAgent) && chrome.permissions) {
-  chrome.permissions.contains({
-    permissions: ['cookies']
-  }, granted => {
-    if (granted === false) {
-      document.querySelector('[data-cmd="container"]').classList.remove('hide');
-    }
-  });
-}
